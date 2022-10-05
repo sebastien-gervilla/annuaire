8d39d9ddda1efa25e9dc557d47c32d6e2d8cea6e
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-05 12:21:37 +0200

    React setup

---
+++ frontend/src/styles/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <section id="home-page">
+            <div className="main-area">
+                <div className="main-content">
+
+                    Hello world
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Home;
