3d5bf9afd8a163248bcb46ca76fd0b2d9de40207
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-05 11:43:54 +0200

    Settings update

--- api/config/settings.php
+++ api/config/settings.php
@@ -1,9 +1,16 @@
 <?php
 
-function getSettings($env = 'developpement')
+function getSettings(string $env = 'developpement')
 {
     $settingsJson = file_get_contents(__DIR__ . './settings.json');
     $settings = json_decode($settingsJson, true);
     $envSettings = $settings[$env];
     return $envSettings;
+}
+
+function getRouteEndpoints(string $url)
+{
+    $routesURL = getSettings()['routesURL'];
+    $endpoints = str_replace($routesURL, '', $url);
+    return $endpoints;
 }
