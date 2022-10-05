919f4e9db31771c65a7a23fda374343f739edc07
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-05 15:19:05 +0200

    Students & styles

---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Login = () => {
+    return (
+        <section id="login-page">
+            <Header />
+
+            <div className="main-area">
+                <div className="main-content">
+
+                    
+
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
