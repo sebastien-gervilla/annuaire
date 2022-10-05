78de7bcf65d93afd408bcb87ee822f9fc6c4c01e
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-05 11:44:33 +0200

    Student controller templates

--- api/controllers/StudentController.php
+++ api/controllers/StudentController.php
@@ -1,9 +1,22 @@
 <?php
 
 class StudentController {
-    public static function students()
-    {
-        $students = "Data";
+    public static function getAll() {
+        $students = array(
+            array(
+                "fname" => "Sébastien",
+                "lname" => "Gervilla"
+            ),
+            array(
+                "fname" => "Killian",
+                "lname" => "Lecorf"
+            )
+        );
+        return $students;
+    }
+
+    public static function getById(int $id) {
+        $students = $id;
         return $students;
     }
 }
