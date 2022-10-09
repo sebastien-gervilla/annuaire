e73149686f6180158b85621587941fbccbeea9b4
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-09 18:50:35 +0200

    Enabled DELETE and PUT methods in Cors

--- api/inc/request-utils.php
+++ api/inc/request-utils.php
@@ -49,5 +49,6 @@ function allowCors() {
     if (isset($_SERVER['HTTP_ORIGIN'])) {
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header("Access-Control-Allow-Credentials: true");
+        header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
     }
 }
