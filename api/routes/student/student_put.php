6ebd73599722f227a7ee1bfc51efd9e3b9bbc413
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-09 13:33:15 +0200

    Added PUT route

---
+++ api/routes/student/student_put.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../Router.php';
+require_once __DIR__ . '/../../inc/Response.php';
+require_once __DIR__ .'/../../controllers/StudentController.php';
+
+function useStudentPutRoutes(string $endpoint, array|null $body): Response
+{
+    switch ($endpoint) {
+        case 'student':
+            $student = $body;
+            $id = $body['_id'];
+            return Router::get(function () use($id, $student) { 
+                return StudentController::modifyStudent($id, $student); 
+            });
+        
+        default:
+            return new Response(400, false, "Couldn't find url endpoint.");
+    }
+}
