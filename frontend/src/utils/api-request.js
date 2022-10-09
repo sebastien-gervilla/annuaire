39c5445cf0cf8688f64168210ab2ea4546709b9b
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-09 12:54:52 +0200

    Fixing request body

--- frontend/src/utils/api-request.js
+++ frontend/src/utils/api-request.js
@@ -1,6 +1,8 @@
-export default async function apiRequest(relUrl, method, body = null, options = {}) {
+export default async function apiRequest(relUrl, method, data = null, options = {}) {
     const fullUrl = process.env.REACT_APP_API_PATH + relUrl;
+
     try {
+        const body = JSON.stringify(data);
         const res = await fetch(fullUrl, {...options, body, method});
         return await res.json();
     } catch (error) {
