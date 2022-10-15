59102a3ad086e03fdd813eb0c0617133dd7cb293
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-15 20:12:29 +0200

    Added temporary dark mode

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoPower } from 'react-icons/io5';
 import { HiOutlineMoon } from 'react-icons/hi';
+import { MdWbSunny } from 'react-icons/md';
 
 const Header = () => {
+
+    const [isDark, setIsDark] = useState(false);
+
+    const handleToggleDarkMode = () => {
+        document.documentElement.classList.contains('dark-mode') ?
+            document.documentElement.classList.remove('dark-mode') :
+            document.documentElement.classList.add('dark-mode');
+        
+        setIsDark(prev => !prev);
+    }
+
+    const displayDarkModeIcon = () => !isDark ?
+        <HiOutlineMoon id='dark-mode_icon' onClick={handleToggleDarkMode} /> :
+        <MdWbSunny id='light-mode_icon' onClick={handleToggleDarkMode} />
+
     return (
         <div className="app-header">
             <div className="header-content">
@@ -10,7 +26,7 @@ const Header = () => {
                     <h1>Annuaire NWS</h1>
                 </div>
                 <div className="buttons">
-                    <button><HiOutlineMoon id='dark-mode_icon' /></button>
+                    <button>{displayDarkModeIcon()}</button>
                     <button><IoPower id='disconnect_icon' /></button>
                 </div>
             </div>
