13a22dc2701846cb8e57fdd9b998b38f54d92c6f
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-16 16:05:04 +0200

    App Student page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import AppStudent from './pages/AppStudent';
+import AppStudents from './pages/AppStudents';
 import Home from './pages/Home';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<AppStudents />} />
+        <Route path='/student/:id' element={<AppStudent />} />
       </Routes>
     </BrowserRouter>
   );
