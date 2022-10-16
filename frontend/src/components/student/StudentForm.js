e1516e713e0a0d01ba9ea78c96b2a268bf5657d8
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-16 14:01:26 +0200

    Updating app and student form styles

--- frontend/src/components/student/StudentForm.js
+++ frontend/src/components/student/StudentForm.js
@@ -25,15 +25,7 @@ const StudentForm = ({ studentInfos, method, closeModal, onSubmit }) => {
         age = (age > 99) ? 99 : age;
         setStudent({...student, [event.target.name]: age});
     }
-
-    const handleTableSelectChanges = values => {
-        const value = values.value;
-        setStudent({
-            ...student, 
-            participations: [...student.participations, value]
-        });
-    };
-
+    
     const onChangeSelectValues = (name, values) => { // clean up
         console.log("stud : ", student);
         setStudent(prev => { 
@@ -74,45 +66,47 @@ const StudentForm = ({ studentInfos, method, closeModal, onSubmit }) => {
                 </div>
             </div>
             <div className="form-input_row">
-                <div className="form-input">
-                    <p>GENRE *</p>
-                    <select name="gender" className='form-select' value={student.gender} onChange={handleChanges}>
-                        <option value="Homme">Homme</option>
-                        <option value="Femme">Femme</option>
-                    </select>
+                <div className="form-input_row">
+                    <div className="form-input">
+                        <p>GENRE *</p>
+                        <select name="gender" className='form-select' value={student.gender} onChange={handleChanges}>
+                            <option value="Homme">Homme</option>
+                            <option value="Femme">Femme</option>
+                        </select>
+                    </div>
+                    <div className="form-input">
+                        <p>AGE *</p>
+                        <input type="number" name="age" value={student.age} onChange={handleChangeAge} />
+                    </div>
                 </div>
-                <div className="form-input">
-                    <p>AGE *</p>
-                    <input type="number" name="age" value={student.age} onChange={handleChangeAge} />
-                </div>
-            </div>
-            <div className="form-input_row">
                 <div className="form-input">
                     <p>EMAIL *</p>
                     <input type="email" name='email' value={student.email} onChange={handleChanges} />
                 </div>
+            </div>
+            <div className="form-input_row">
                 <div className="form-input">
                     <p>TELEPHONE</p>
                     <input type="text" name='phone' value={student.phone} onChange={handleChanges} />
                 </div>
-            </div>
-            <div className="form-input">
-                <p>DIPLOME</p>
-                <input type="text" name='degree' value={student.degree} onChange={handleChanges} />
+                <div className="form-input">
+                    <p>DIPLOME</p>
+                    <input type="text" name='degree' value={student.degree} onChange={handleChanges} />
+                </div>
             </div>
             <div className="form-input">
                 <p>ANNEE D'ENTREE</p>
-                <TableSelect name={'school_year'} 
+                <TableSelect name={'entry_years'} 
                     tableData={schoolYearsReq.data?.body} 
                     onChangeValues={onChangeSelectValues}
-                    defaultValues={student.school_year} />
+                    defaultValues={student.entry_years} />
             </div>
             <div className="form-input">
                 <p>POTENTIELLE SPECIALISATION</p>
-                <TableSelect name={'specializations'} 
+                <TableSelect name={'pathways'} 
                     tableData={specializationsReq.data?.body} 
                     onChangeValues={onChangeSelectValues}
-                    defaultValues={student.specializations} />
+                    defaultValues={student.pathways} />
             </div>
             <div className="form-input">
                 <p>PARTICIPATIONS</p>
