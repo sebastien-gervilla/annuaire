2fdebcf7fb0f447133ad9d08fa6faf1490f18872
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-19 14:44:50 +0200

    Fixing useAuth to properly refresh

--- frontend/src/hooks/useAuth.js
+++ frontend/src/hooks/useAuth.js
@@ -7,7 +7,11 @@ export default function useAuth() {
     const navigate = useNavigate();
 
     useEffect(() => {
+        console.log(authReq);
+        console.log(document.cookie);
         if (!authReq.data?.status) return;
         if (authReq.data.status !== 200) navigate('/login');
     }, [authReq.data]);
+
+    return { refresh: () => authReq.doFetch('auth/isauth/?' + document.cookie) }
 }
