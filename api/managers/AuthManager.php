700c30d21f7b1f4b529355e856fccf9723dc0950
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-19 10:32:29 +0200

    Added Auth Manager

---
+++ api/managers/AuthManager.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../config/DatabaseHandler.php';
+require_once __DIR__ . '/../utils/utils.php';
+
+class AuthManager {
+
+    #region GET
+
+    public static function getColumnsNames() {
+        $dbh = DatabaseHandler::connect();
+        $request = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS 
+        WHERE TABLE_SCHEMA = 'annuaire_nws' AND TABLE_NAME = 'user'";
+        $columns = $dbh->query($request)->fetchAll(PDO::FETCH_ASSOC);
+        $columnNames = [];
+        foreach ($columns as $column) {
+            if ($column['COLUMN_NAME'] !== '_id') {
+                array_push($columnNames, $column['COLUMN_NAME']);
+            }
+        }
+        return $columnNames;
+    }
+
+    public static function getAllUsersRequest() {
+        $dbh = DatabaseHandler::connect();
+        $request = "SELECT * FROM user ORDER BY `_id`;";
+        return $dbh->query($request)->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    #endregion
+
+}
