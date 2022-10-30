c5bdbb907773a84097b83e7ff0ed74ef76e2af93
Sébastien Gervilla <sebastiengervilla.c@gmail.com>
2022-10-30 11:43:47 +0100

    Changing user test to admin test

--- tests/index.php
+++ tests/index.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once './utils/TestHandler.php';
-require_once './inc/user-test.php';
+require_once './inc/admin-test.php';
 
 // User table should always have atleast one user
 
-TestHandler::run("User table should have atleast one user.", function (callable $expect) {
-    return $expect(isUserTableEmpty())->toBe(false);
+TestHandler::run("User table should have atleast one admin.", function (callable $expect) {
+    return $expect(hasAdmin())->toBe(true);
 });
